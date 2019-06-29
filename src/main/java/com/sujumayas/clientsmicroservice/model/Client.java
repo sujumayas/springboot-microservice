0f33b8d62aa94d23887c1d152e10035cfa9769e1
Esen Espinosa <sujumayas@gmail.com>
2019-06-29 13:38:36 -0500

    Refactors client model to be full entity for tpa to handle db

--- src/main/java/com/sujumayas/clientsmicroservice/model/Client.java
+++ src/main/java/com/sujumayas/clientsmicroservice/model/Client.java
@@ -1,18 +1,23 @@
 package com.sujumayas.clientsmicroservice.model;
 
-import java.util.UUID; /** To be used when we refactor UUID ids to be auto-generated and unique */
-
+import javax.annotation.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Client base Model
  */
 @Entity
+@Table(name = "clients")
 public class Client {
 
     @Id
-    private String id;
+    private long id;
+    
     private String name;
     private String lastName;
     private String age;
@@ -23,16 +28,16 @@ public class Client {
     }
 
     /** Main Constructor */
-    public Client(String id, String name, String lastName, String age, String birthDate) {
-        super();
-        this.id = id;
+    public Client(String name, String lastName, String age, String birthDate) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
         this.birthDate = birthDate;
     }
 
-    public String getId() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
         return id;
     }
 
@@ -40,6 +45,7 @@ public class Client {
         this.id = id;
     }
 
+    @Column(name = "first_name", nullable = false)
     public String getName() {
         return name;
     }
@@ -47,7 +53,8 @@ public class Client {
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    @Column(name = "last_name", nullable = false)
     public String getLastName() {
         return lastName;
     }
@@ -56,6 +63,7 @@ public class Client {
         this.lastName = lastName;
     }
 
+    @Column(name = "age", nullable = false)
     public String getAge() {
         return age;
     }
@@ -64,12 +72,19 @@ public class Client {
         this.age = age;
     }
 
+    @Column(name = "birth_date", nullable = false)
     public String getBirthDate() {
         return birthDate;
     }
 
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString(){
+        return "Cliente: { id: "+ id + ", Name: "+ name +", LastName: "+lastName+", Age: "+age+", BirthDate: "+birthDate+" }"; 
+
     }
 
     
