51c35a1725c54d476e29a16a7fb2cc5392352c3d
Esen Espinosa <sujumayas@gmail.com>
2019-07-01 13:40:36 -0500

    changed path of dockerfile

--- jenkins/scripts/build_docker.sh
+++ jenkins/scripts/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo $1
+cat $1
 echo '$1'
 
 curl -XPOST --unix-socket /var/run/docker.sock -d '{"Image":"nginx"}' -H 'Content-Type: application/json' http://localhost/containers/create
