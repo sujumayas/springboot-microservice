6dd55cd013f6bb54f8498068c303377cdf84128f
Esen Espinosa <sujumayas@gmail.com>
2019-07-01 14:09:41 -0500

    Crated second curl

--- jenkins/scripts/build_docker.sh
+++ jenkins/scripts/build_docker.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-
 cat $1
-echo '$1'
 
-curl -XPOST --unix-socket /var/run/docker.sock -d '{"Image":"sujumayas/springboot-test"}' -H 'Content-Type: application/json' http://localhost/containers/create
+ID=$(curl -XPOST --unix-socket /var/run/docker.sock -d '{"Image":"sujumayas/springboot-test"}' -H 'Content-Type: application/json' http://localhost/containers/create | grep -oP '(?<="Id": ")[^"]*')
+
+echo "$ID"
+
+curl -XPOST --unix-socket /var/run/docker.sock "http://localhost/containers/$ID/start"
+
